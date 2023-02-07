bfe151b56b439a8948cf371ca7ea0f9ad3c99010
pgmar257 <122751130+pgmar257@users.noreply.github.com>
2023-02-07 09:19:35 +0100

    Add files via upload

---
+++ src.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+data_actor = pd.read_csv('../data/actor.csv')
+data_category = pd.read_csv('../data/category.csv')
+data_film = pd.read_csv('../data/film.csv')
+data_inventory = pd.read_csv('../data/inventory.csv')
+data_language = pd.read_csv('../data/language.csv')
+data_old_HDD = pd.read_csv('../data/old_HDD.csv')
+data_rental = pd.read_csv('../data/rental.csv')
+
